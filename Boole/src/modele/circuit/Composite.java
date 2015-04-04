9d7ba342c8d8ec9d276542dc05411f3142ea3af0
Pierrick <pierrickjabrun@gmail.com>
2015-04-05 01:08:31 +0200

    circuit ouvert et composite théoriquement fonctionnels

--- Boole/src/modele/circuit/Composite.java
+++ Boole/src/modele/circuit/Composite.java
@@ -1,30 +1,29 @@
 package modele.circuit;
 
+import java.util.LinkedHashSet;
+import java.util.Map.Entry;
+
 import exception.EndException;
 import exception.StateException;
 import modele.Composant;
 import modele._Composant;
+import modele.port.Entree;
 
 public class Composite extends Ouvert implements _Composant {
 
-	public Composite(Composant composant) {
-		super(composant);
-	}
-
-	@Override
-	public void traitement() {
-		// TODO Auto-generated method stub
-
-		/*
-		 * Composite c = new Composite(); _Composant c2 = c; Composant c3 =
-		 * (Composant)c2;
-		 */
+	public Composite(LinkedHashSet<Composant> premiers,
+			LinkedHashSet<Composant> derniers) {
+		super(premiers, derniers);
 	}
 
 	@Override
 	public void tryTraitement() throws EndException, StateException {
-		// TODO Auto-generated method stub
-
+		for (Entry<Entree, Boolean> entry : this.inList.entrySet()) {
+			if (entry.getValue().booleanValue() == false) {
+				return;
+			}
+		}
+		traitement();
 	}
 
 }
--- Boole/src/modele/circuit/Ouvert.java
+++ Boole/src/modele/circuit/Ouvert.java
@@ -1,8 +1,7 @@
 package modele.circuit;
 
+import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
-import java.util.LinkedList;
-import java.util.List;
 
 import modele.Circuit;
 import modele.Composant;
@@ -12,8 +11,8 @@ import modele.port.Sortie;
 
 public class Ouvert extends Circuit {
 
-	protected List<Entree> Entrees = new LinkedList<Entree>();
-	protected List<Sortie> Sorties = new LinkedList<Sortie>();
+	protected LinkedHashMap<Entree, Boolean> inList = new LinkedHashMap<Entree, Boolean>();
+	protected LinkedHashSet<Sortie> outList = new LinkedHashSet<Sortie>();
 
 	public Ouvert(LinkedHashSet<Composant> premiers,
 			LinkedHashSet<Composant> derniers) {
@@ -23,7 +22,7 @@ public class Ouvert extends Circuit {
 				composant = (Transformateur) composant;
 				for (Entree entree : ((Transformateur) composant).getInList()
 						.keySet()) {
-					Entrees.add(entree);
+					inList.put(entree, new Boolean(null));
 				}
 			}
 		}
@@ -31,7 +30,7 @@ public class Ouvert extends Circuit {
 			if (composant instanceof Transformateur) {
 				composant = (Transformateur) composant;
 				for (Sortie sortie : ((Transformateur) composant).getOutList()) {
-					Sorties.add(sortie);
+					outList.add(sortie);
 				}
 			}
 		}
